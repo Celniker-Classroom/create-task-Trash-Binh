d16a1348854fa00bc3a6e96b6cbc4b71cb52329f
Binh Hoang <hoangb3299@my.sduhsd.net>
2026-04-15 21:15:19 +0000

    Today I worked on story branch logic, using if statements and boolean values to determine the flow of the story for the first 2 story branches

--- script.js
+++ script.js
@@ -1,61 +1,105 @@
+
+// boolean values to change functions for branches, 
+// set them true and then slowly turn the off for each step
+let branch1Run = true;
+let branch2Run = true;
+let branch3Run = true;
+
 // button variables
 let startGame = document.getElementById("startGame");    // starts the game
 startGame.addEventListener("click", gameBegin);
 
-let choice1 = document.getElementById("1");    // goodchoice
-choice1.addEventListener("click", goodAdd);
+let choiceGood = document.getElementById("good");    // goodchoice
+choiceGood.addEventListener("click", goodAdd);
+let choiceHmm = document.getElementById("hmm");    // badchoice
+choiceHmm.addEventListener("click", bothAdd);
 
-let choice2 = document.getElementById("2");    // badchoice
-choice2.addEventListener("click", randomAdd);
-
-let choice3 = document.getElementById("3");    // mysterychoice
-choice3.addEventListener("click", badAdd);
+let choiceBad = document.getElementById("bad");    // mysterychoice
+choiceBad.addEventListener("click", badAdd);
 
 
 //buttons that are disabled when started
-choice1.disabled = true;   
-choice2.disabled = true;  
-choice3.disabled = true;  
+choiceGood.disabled = true;   
+choiceHmm.disabled = true;  
+choiceBad.disabled = true;  
 
 //arrays added for game ending
 let goodEnd = [];
 let badEnd = [];
 
-///what happens when the game begins
+/// STORY BRANCHES
 function gameBegin(){
 
 //renable submit choice buttons
-choice1.disabled = false;  
-choice2.disabled = false;   
-choice3.disabled = false;
-startGame.disabled= true;
+choiceGood.disabled = false;  
+choiceHmm.disabled = false;   
+choiceBad.disabled = false;
+startGame.disabled = true;
 
 //update text
 document.getElementById("storytext").textContent = "Hey there! I'm Silly and I need to protect my friends at the seal village from the evil polar bears! Which path should I take to reach their evil lair?";
-document.getElementById("1").textContent = "The Hail-Shower Hills";
-document.getElementById("2").textContent = "The Snowflower Snowfields";
-document.getElementById("3").textContent = "The Ice-Shard Cavern";
+document.getElementById("good").textContent = "The Hail-Shower Hills";
+document.getElementById("hmm").textContent = "The Snowflower Snowfields";
+document.getElementById("bad").textContent = "The Ice-Shard Cavern";
+
 }
 
+function storyBranch1(){
+//update text
+document.getElementById("storytext").textContent = "Now that Silly has traveled through the chosen landscape, she needs to rest now to regain her energy at a nearby town. Where is she sleeping tonight?";
+document.getElementById("good").textContent = "The PeinguINN";
+document.getElementById("hmm").textContent = "At a small campsite";
+document.getElementById("bad").textContent = "No sleeping for Silly, continue on with the journey!";
+// branch 1 stops here
+branch1Run = false;
+}
 
-// arrays addition
+function storyBranch2(){
+//update text
+document.getElementById("storytext").textContent = "Silly has now reached the evil polar bear lair. How will she able to prevent them from attack the village";
+document.getElementById("good").textContent = "Seal-style karate!";
+document.getElementById("hmm").textContent = "The power of friendship!";
+document.getElementById("bad").textContent = "A flamethrower!";
+// branch 2 stops here
+branch2Run = false;
+}
 
 //good add
 function goodAdd(){
-goodEnd.push(1);
-
+goodEnd.push(Math.random()*5);
+//branch story determine
+if (branch1Run === true){
+storyBranch1();
+} else if(branch2Run === true){
+    storyBranch2();
+}
 }
 
 //random add
-function randomAdd(){
+function bothAdd(){
+badEnd.push(Math.random()*5);
+goodEnd.push(Math.random()*5);
 
+if (branch1Run === true){
+storyBranch1();
+} else if(branch2Run === true){
+    storyBranch2();
 }
-
+}
 
 //bad add
 function badAdd(){
-
+badEnd.push(Math.random()*5);
+if (branch1Run === true){
+storyBranch1();
+} else if(branch2Run === true){
+    storyBranch2();
 }
+}
+
+
+
+
 
 
 
