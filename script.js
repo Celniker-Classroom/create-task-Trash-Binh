ff0f8177e608b0c951552bb50aaaad22e8167a9e
Binh Hoang <hoangb3299@my.sduhsd.net>
2026-04-23 20:52:46 +0000

    Today I worked on centering the my CSS and added option choices after you pick either a good or bad choice.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// MISCELANOUS VALUES
 // boolean values to change functions for branches,
 // set them true and then slowly turn the off for each step
 let branch1Run = true;
@@ -10,12 +10,20 @@ let winDetermine = 0;
 let loseDetermine = 0;
 
 
-/// gamblEnding count
+/// special Ending count
 let gambleCount = 0;
-
+let pureCount = 0;
+let  unpureCount =0;
 // button variables
 let startGame = document.getElementById("startGame");    // starts the game
 startGame.addEventListener("click", gameBegin);
+
+//arrays added for game ending
+let theEnd = [];
+
+// out come select
+let firstChoice = theEnd[0];
+
 
 
 // AI FIXED CODE using ChatGPT by using function(){arrDelay}
@@ -23,8 +31,6 @@ let choiceGood = document.getElementById("good");    // goodchoice
 choiceGood.addEventListener("click", function() {arrAdd("good")});
 let choiceHmm = document.getElementById("hmm");    // hmm choice
 choiceHmm.addEventListener("click", function() {arrAdd("hmm")})
-
-
 let choiceBad = document.getElementById("bad");    // bad choice
 choiceBad.addEventListener("click", function() {arrAdd("bad")})
 // AI fixed code ends here
@@ -36,8 +42,6 @@ choiceHmm.disabled = true;
 choiceBad.disabled = true;  
 
 
-//arrays added for game ending
-let theEnd = [];
 
 
 /// STORY BRANCHES
@@ -63,11 +67,26 @@ document.getElementById("bad").textContent = "The Ice-Shard Mountain, a tall sha
 
 
 function storyBranch1(){
-//update text
+// 1st choices result
+if ((firstChoice = "good") && (pureCount = 1)){ // good choice, good result
+ document.getElementById("resultChoice").textContent = "Silly scoots along the Snow-Spring Hill peacfully. On her way, she found a lot of rare herbal medicine (extremly rare in the Artic) that she could use!"
+}
+else if ((firstChoice = "good") && (gambleCount = 1)){
+  document.getElementById("resultChoice").textContent = "Silly scoots into the tunnel. She ends up relatively unscathed as a result and now is able to rest earlier!" ; 
+}
+else if ((firstChoice = "bad") && (gambleCount = 1)){
+  document.getElementById("resultChoice").textContent = "Silly scoots into the tunnel. She ends up relatively unscathed as a result and now is able to rest earlier!" ; 
+}
+
+
+
+ //update text
 document.getElementById("storytext").textContent = "Now that Silly has traveled through the chosen landscape, she needs to rest now to regain her energy at a nearby town. Where is she sleeping tonight?";
 document.getElementById("good").textContent = "The PeinguINN, a local hotel with fair ratings!";
 document.getElementById("hmm").textContent = "At a small campsite, known for its spirits that curse or bless visitors!";
 document.getElementById("bad").textContent = "No sleeping for Silly, continue on with the journey!";
+
+
 // branch 1 stops here
 branch1Run = false;
 }
@@ -107,6 +126,7 @@ function storyEnd(listArray){ // determine base outcome
 
 specialEnd();
 
+
 // reset choices
     branch1Run = true;
     branch2Run = true;
@@ -116,25 +136,32 @@ specialEnd();
     startGame.disabled = false;
     theEnd = [];
     winDetermine = 0;
-    loseDetermine = 0; 
+    loseDetermine = 0;
+    gambleCount= 0;
+    pureCount =0;
+    unpureCount = 0; 
 }
     
 
  
-function specialEnd(){ // checks and update for special eding
-    if(theEnd.join(",") === ("good,good,good")){ //array.join from w3schools: https://www.w3schools.com/jsref/jsref_join.asp
+function specialEnd(){ // checks and update for special endings
+ if(gambleCount===3){ //gamble ending, achieved by clicking all hmm options
+    document.getElementById("gameInst").textContent = "Rare Special Ending: Super Gamble End! Press the start button to play again";
+    document.getElementById("storytext").textContent = "A lot of bad choices were defnitely here. Not only was Silly tired and exhausted when coming to the Polar Bear lair, she also accidently turned the flamethrower on the highest heat due to her being exhausted! As a result, not only did she melted the polar bear's homes but also the entire artics's as well! Whoopsies!";
+    document.getElementById("gambleEnd").textContent = "Super Gamble Ending: Achieved";   
+    }
+    else if(pureCount===3){ //super good ending, achieved by click all good options
     document.getElementById("gameInst").textContent = "Rare Special Ending: Super Good End! Press the start button to play again";
     document.getElementById("storytext").textContent = "Not only did you prevent the polar bears from attacking, you were so smart that they even forfeited all of their kingdom's money and gave it to the Seal Village!";
     document.getElementById("pureEnd").textContent = "Super Good Ending: Achieved";
 }
-else if(theEnd.join(",")=== ("bad,bad,bad")){
+    else if(unpureCount===3){ //super bad ending, achieed by click all bad options
     document.getElementById("gameInst").textContent = "Rare Special Ending: Super Bad End! Press the start button to play again";
-    document.getElementById("storytext").textContent = "A lot of bad choices were defnitely here. Not only was Silly tired and exhausted when coming to the Polar Bear lair, she also accidently turned the flamethrower on the highest heat due to her being exhausted! As a result, not only did she melted the polar bear's homes but also the entire artics's as well! Whoopsies!";
+    document.getElementById("storytext").textContent = "A lot of bad choices were defnitely here. Not only was Silly tired and exhausted when coming to the Polar Bear lair, she also accidently turned on her flamethrower on the highest heat setting due to her being exhausted! As a result, not only did she melted the polar bear's homes but also the entire artics's as well! Whoopsies!";
     document.getElementById("unpureEnd").textContent = "Super Bad Ending: Achieved";  
+
 }
-    else{
-    
-    }
+
 }
 
 
@@ -145,7 +172,8 @@ function arrAdd(choice){
 // debugged logic issues using ChatGPT, specifcally for the button values "hmm" input and "bad" input errors by identifying typo of not using "ëlse" in if else
     if(choice === "good"){  //good input
         theEnd.push("good");
-}
+   pureCount++;
+    }
     else if(choice === "hmm"){ 
         let riskOutcome = Math.floor(Math.random()*2);
             if(riskOutcome === 1){
@@ -154,9 +182,11 @@ function arrAdd(choice){
             else{ //bad input
                 theEnd.push("bad");
             }
+    gambleCount++;
 }
     else{ 
     theEnd.push("bad");
+       unpureCount++;
 }
 
 
